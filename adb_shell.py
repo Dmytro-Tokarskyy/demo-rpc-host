7757ebd8d55caf9e91079a4c39b20ac6e7f40857
Dmytro Tokarskyy <dmytro.tokarskyy@globallogic.com>
2021-12-15 19:25:31 +0200

    Make commands via JSON-RPC

--- adb_shell.py
+++ adb_shell.py
@@ -1,4 +1,11 @@
+import json
+import random
 import subprocess
+
+ACTION_DISCOVER = "com.example.devicerpc.action.DISCOVER"
+ACTION_PAIR = "com.example.devicerpc.action.PAIR"
+ACTION_UNPAIR = "com.example.devicerpc.action.UNPAIR"
+ACTION_COMMAND = "com.example.devicerpc.action.COMMAND"
 
 
 def install_apk(device, apk_path):
@@ -10,9 +17,24 @@ def send_intent(device, action, data=""):
     if data:
         data = f' --es data "{data}"'
     command = f'adb -s {device} shell am broadcast -a {action}{data}'
+    print(command)
     print(subprocess.getoutput(command))
 
 
 def launch_app(device, package):
     command = f'adb -s {device} shell am start -n "{package}"/.MainActivity'
     print(subprocess.getoutput(command))
+
+
+def send_rpc_command(device, method, params=""):
+    command = {"jsonrpc": "2.0", "method": method, "params": params if params else "-", "id": str(random.randint(1, 1000))}
+    command = encode_intent_data(command)
+    send_intent(device, ACTION_COMMAND, command)
+
+
+def encode_intent_data(message):
+    payload = json.dumps(message)
+    payload = f'{payload}'
+    payload = payload.translate(str.maketrans({" ": r"\ ", "{": r"\{", "}": r"\}", "\\": r"\\", "'": r"\'", r'"': r'\"'}))
+    print(payload)
+    return payload
--- main.py
+++ main.py
@@ -10,10 +10,6 @@ DEVICE_2_MAC = '34:14:5F:AC:43:88'
 
 PACKAGE = "com.example.devicerpc"
 
-ACTION_DISCOVER = "com.example.devicerpc.action.DISCOVER"
-ACTION_PAIR = "com.example.devicerpc.action.PAIR"
-ACTION_UNPAIR = "com.example.devicerpc.action.UNPAIR"
-
 OK_BUTTON1 = "com.android.settings:id/button1"
 OK_BUTTON2 = "android:id/button1"
 
@@ -26,23 +22,23 @@ def main():
     adb.launch_app(DEVICE_2_SERIAL, PACKAGE)
 
     # Send intent for discoverable to devices
-    adb.send_intent(DEVICE_1_SERIAL, ACTION_DISCOVER)
+    adb.send_rpc_command(DEVICE_1_SERIAL, "discover") # adb.send_intent(DEVICE_1_SERIAL, adb.ACTION_DISCOVER)
     device1_ui = uiautomator2.connect(DEVICE_1_SERIAL)
     device1_ui(resourceId=OK_BUTTON1).click()
 
-    adb.send_intent(DEVICE_2_SERIAL, ACTION_DISCOVER)
+    adb.send_rpc_command(DEVICE_2_SERIAL, "discover")
     device2_ui = uiautomator2.connect(DEVICE_2_SERIAL)
     device2_ui(resourceId=OK_BUTTON2).click()
 
     # From device 1 send pair request to device 2
-    adb.send_intent(DEVICE_1_SERIAL, ACTION_PAIR, DEVICE_2_MAC)
+    adb.send_rpc_command(DEVICE_1_SERIAL, "pair", f'"{DEVICE_2_MAC}"') #adb.send_intent(DEVICE_1_SERIAL, adb.ACTION_PAIR, DEVICE_2_MAC)
 
     # # Accept pairing on devices
     device1_ui(resourceId=OK_BUTTON1).click()
     device2_ui(resourceId=OK_BUTTON2).click()
 
-    adb.send_intent(DEVICE_1_SERIAL, ACTION_UNPAIR, DEVICE_2_MAC)
-    adb.send_intent(DEVICE_2_SERIAL, ACTION_UNPAIR, DEVICE_1_MAC)
+    adb.send_rpc_command(DEVICE_1_SERIAL, "unpair", f'"{DEVICE_2_MAC}"') #adb.send_intent(DEVICE_1_SERIAL, adb.ACTION_UNPAIR, DEVICE_2_MAC)
+    adb.send_rpc_command(DEVICE_2_SERIAL, "unpair", f'"{DEVICE_2_MAC}"') #adb.send_intent(DEVICE_2_SERIAL, adb.ACTION_UNPAIR, DEVICE_1_MAC)
 
 
 if __name__ == '__main__':
